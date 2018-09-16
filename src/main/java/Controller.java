a601ad7e48d01bcae0e9e0ceb5e05a8ecb6a06ea
JackTongen <tongenjack@gmail.com>
2018-09-16 11:20:26 +0200

    Add all button to the controller

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,5 +1,89 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
-    public Controller() {
-        System.out.println("Controller");
+
+    @FXML
+    private TextField scoreTextField;
+
+    @FXML
+    private Button percentButton;
+
+    @FXML
+    private Button ceButton;
+
+    @FXML
+    private Button cButton;
+
+    @FXML
+    private Button backspaceButton;
+
+    @FXML
+    private Button divideButton;
+
+    @FXML
+    private Button radicalButton;
+
+    @FXML
+    private Button sevenButton;
+
+    @FXML
+    private Button eightButton;
+
+    @FXML
+    private Button nineButton;
+
+    @FXML
+    private Button multiButton;
+
+    @FXML
+    private Button squaredXButton;
+
+    @FXML
+    private Button fourButton;
+
+    @FXML
+    private Button fiveButton;
+
+    @FXML
+    private Button sixButton;
+
+    @FXML
+    private Button minusButton;
+
+    @FXML
+    private Button xPower3Button;
+
+    @FXML
+    private Button oneButton;
+
+    @FXML
+    private Button twoButton;
+
+    @FXML
+    private Button threeButton;
+
+    @FXML
+    private Button plusButton;
+
+    @FXML
+    private Button oneForXButton;
+
+    @FXML
+    private Button plusMinusButton;
+
+    @FXML
+    private Button zeroButton;
+
+    @FXML
+    private Button pointButton;
+
+    @FXML
+    private Button equalButton;
+
+    @FXML
+    void initialize(){
+        scoreTextField.setText("0");
     }
 }
