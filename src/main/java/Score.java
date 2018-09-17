1addc445224b45e18bef651367b5e0e75bedfbf4
JackTongen <tongenjack@gmail.com>
2018-09-17 17:34:52 +0200

    Add Score class

---
+++ src/main/java/Score.java
@@ -0,0 +1,34 @@
+public class Score {
+    private float firstArgument;
+    private float secondArgument;
+    private int symbol;
+
+    public float getFirstArgument() {
+        return firstArgument;
+    }
+
+    public void setFirstArgument(float firstArgument) {
+        this.firstArgument = firstArgument;
+    }
+
+    public float getSecondArgument() {
+        return secondArgument;
+    }
+
+    public void setSecondArgument(float secondArgument) {
+        this.secondArgument = secondArgument;
+    }
+
+    public int getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(int symbol) {
+        this.symbol = symbol;
+    }
+
+
+
+    public Score() {
+    }
+}
